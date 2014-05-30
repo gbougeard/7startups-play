6fc14c90ee6fbba8b531761a1c540d1eb9f5499b
Greg Bougeard <gbougeard@gmail.com>
2014-05-30 14:37:05 +0200

    generate Age1 player states

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -41,16 +41,16 @@ object Game {
   def deal(age: Age, nbPlayers: PlayerCount): Map[PlayerId, List[RegularCard]] = {
 
     def filterByAgeAndPlayerCount(c: RegularCard): Boolean = (c.cAge == age) && (c.cMinPlayers <= nbPlayers)
-    def filterByAge(c: RegularCard): Boolean = (c.cAge == age)
+    def filterByAge(c: RegularCard): Boolean = c.cAge == age
 
     val regularCards = allCards.filter(filterByAgeAndPlayerCount) ++ addCommunities(age, nbPlayers)
-//    play.Logger.debug(s"total : ${allCards.size} byAge :  ${allCards.filter(filterByAge).size} filtered : ${allCards.filter(filterByAgeAndPlayerCount).size} communities : ${addCommunities(age, nbPlayers).size} => for $nbPlayers you have ${regularCards.size} cards")
+    //    play.Logger.debug(s"total : ${allCards.size} byAge :  ${allCards.filter(filterByAge).size} filtered : ${allCards.filter(filterByAgeAndPlayerCount).size} communities : ${addCommunities(age, nbPlayers).size} => for $nbPlayers you have ${regularCards.size} cards")
 
-//    allCards.filter(filterByAgeAndPlayerCount).map(println)
+    //    allCards.filter(filterByAgeAndPlayerCount).map(println)
     Random.setSeed(new DateTime().getMillis)
     val shuffleCards = Random.shuffle(regularCards.toList)
     val hands = shuffleCards.grouped(7).toList
-//    play.Logger.debug(s"hands $hands")
+    //    play.Logger.debug(s"hands $hands")
     playerList(nbPlayers).zip(hands).toMap[PlayerId, List[RegularCard]]
 
   }
@@ -58,6 +58,29 @@ object Game {
   def playerList(nbPlayers: PlayerCount): Set[PlayerId] = {
     val players = for (i <- 1 to nbPlayers) yield s"Player$i"
     players.toSet
+  }
+
+  def init(nbPlayers: PlayerCount) = {
+    val companies: Map[PlayerId, CompanyCard] = dealCompanies(nbPlayers)
+    val players: Set[PlayerId] = playerList(nbPlayers)
+    val age1Hand: Map[PlayerId, List[RegularCard]] = deal(Age1, nbPlayers)
+
+    val age1PlayerStates = players.map(player => PlayerState(pCompany = companies.get(player).get.cCompany,
+                                                             pCompanyStage = Project,
+                                                             pCards = age1Hand.get(player).get,
+                                                             pFunds = 3,
+                                                             pNeighborhood = getNeighborhood(players.toList, player),
+                                                             pPoachingResults = Set()))
+
+  }
+
+  def getNeighborhood(players : List[PlayerId], player:PlayerId): Neighborhood = {
+    val rightIdx = players.indexOf(player) +1
+    val leftIdx = players.indexOf(player) -1
+
+    val right = if (rightIdx > players.size) players.head else players(rightIdx)
+    val left = if (leftIdx < 0) players.last else players(leftIdx)
+    (left, right)
   }
 
 }
--- app/models/GameTypes.scala
+++ app/models/GameTypes.scala
@@ -3,6 +3,7 @@ package models
 import models.Base._
 import models.Cards._
 import scala.util.Random
+import scalaz.MonadState
 
 /**
  * Created by gbougeard on 22/05/14.
@@ -15,13 +16,13 @@ object GameTypes {
 
   case class PlayerState(pCompany: CompanyProfile,
                          pCompanyStage: CompanyStage,
-                         pCards: Set[Card],
+                         pCards: List[RegularCard],
                          pFunds: Funding,
-                         pNeighborhodd: Neighborhood,
+                         pNeighborhood: Neighborhood,
                          pPoachingResults: Set[PoachingOutcome])
 
   case class GameState(playerMap: Map[PlayerId, PlayerState],
-                       discardPile: Set[Card],
+                       discardPile: List[RegularCard],
                        rnd: Random)
 
   sealed trait ActionType
@@ -29,10 +30,10 @@ object GameTypes {
   case object Drop extends ActionType
   case object BuildCompany extends ActionType
 
-  case class PlayerAction(actionType: ActionType, card: Card)
+  case class PlayerAction(actionType: ActionType, card: RegularCard)
 
-//  type NonInteractive = MonadState[_, GameState]
-//  type GameStateOnly = MonadState[_, GameState]
+  type NonInteractive = MonadState[GameState, Int]
+  type GameStateOnly = MonadState[GameState, Int]
 
   sealed trait Communication
   case class RawMessage(msg: String) extends Communication
